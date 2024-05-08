ea8724ef03909fd7e65c126a02b6a4d3ef9703b9
Poojaboyapelly <pooja.boyapelly@bizacuity.com>
2024-05-08 13:48:05 +0530

    added logout button

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './Components/Navbar/Navbar';
 import Landing_Page from './Components/Landing_Page/Landing_Page';
-
-
+import Login from "./Components/Login/Login";
+import SignUp from "./Components/Sign_Up/Sign_Up";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
@@ -13,6 +14,8 @@ function App() {
           <Navbar/>
               <Routes>
               <Route path="/" element={<Landing_Page/>}/>
+              <Route path="/Login" element={<Login/>}/>
+              <Route path="/SignUp" element={<SignUp/>}/>
               </Routes>
             
         </BrowserRouter>
