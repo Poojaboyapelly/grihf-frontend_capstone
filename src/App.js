0a75b7734e1e774872856c035a3976d8f8d1277b
Poojaboyapelly <pooja.boyapelly@bizacuity.com>
2024-05-09 15:30:43 +0530

    added booking consultation,application form

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ import Navbar from './Components/Navbar/Navbar';
 import Landing_Page from './Components/Landing_Page/Landing_Page';
 import Login from "./Components/Login/Login";
 import SignUp from "./Components/Sign_Up/Sign_Up";
+import InstantConsultation from './Components/InstantConsultationBooking/InstantConsultation';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Healthblog from './Components/Home/Home';
+import DoctorCard from './Components/DoctorCard/DoctorCard';
+import BookingConsultation from './Components/BookingConsultation';
+
 
 function App() {
   return (
@@ -16,6 +21,10 @@ function App() {
               <Route path="/" element={<Landing_Page/>}/>
               <Route path="/Login" element={<Login/>}/>
               <Route path="/SignUp" element={<SignUp/>}/>
+              <Route path="/InstantConsultation" element={<InstantConsultation/>} />
+              <Route path='/Home' element ={<Healthblog/>}/>
+              <Route path='/BookingConsultation' element={<BookingConsultation/>}/>
+
               </Routes>
             
         </BrowserRouter>
